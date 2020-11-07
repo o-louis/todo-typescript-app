cd624865c9df6261cf5159a4a9d9049794c96692
Oriane <oriane.louis.pro@gmail.com>
2020-11-07 01:16:12 +0100

    style(TodoListItem): Add style on the input and the svg icons for the 2 actions

--- src/components/TodoListItem.tsx
+++ src/components/TodoListItem.tsx
@@ -1,4 +1,7 @@
 import React from "react";
+// Modules
+import styled from "styled-components";
+import { AiOutlineEdit, AiOutlineDelete } from "react-icons/ai";
 
 export interface Todo {
   id: number;
@@ -13,27 +16,73 @@ type Props = {
   toggleCompleted: (todo: Todo) => void;
 };
 
+const TodoItem = styled.li`
+  display: flex;
+  align-items: center;
+  font-size: 1rem;
+  &:hover {
+    background: #f3fbfb;
+  }
+
+  input {
+    display: none;
+  }
+`;
+
+const Label = styled.label`
+  flex: 1 1 100%;
+  padding: 10px;
+  cursor: pointer;
+
+  &.done {
+    text-decoration: line-through;
+    color: #a2a4b0;
+  }
+`;
+
+const ButtonIcon = styled.button`
+  font-size: 1.3rem;
+  margin-left: 20px;
+
+  &:hover svg {
+    color: #4299e1;
+    cursor: pointer;
+  }
+
+  &:nth-of-type(2) {
+    margin-right: 10px;
+    &:hover svg {
+      color: #e53e3e;
+      cursor: pointer;
+    }
+  }
+`;
+
 const TodoListItem: React.FC<Props> = ({
   todo,
   deleteTodo,
   editTodo,
   toggleCompleted,
 }) => (
-  <li>
+  <TodoItem>
     <input
       type="checkbox"
       id={`checkbox-${todo.id}`}
       onChange={() => toggleCompleted(todo)}
       checked={todo.completed}
     />
-    <label
+    <Label
       htmlFor={`checkbox-${todo.id}`}
       className={todo.completed ? "done" : undefined}
       dangerouslySetInnerHTML={{ __html: todo.name }}
     />
-    <button onClick={() => editTodo(todo)}>EDIT</button>
-    <button onClick={() => deleteTodo(todo)}>DELETE</button>
-  </li>
+    <ButtonIcon onClick={() => editTodo(todo)}>
+      <AiOutlineEdit />
+    </ButtonIcon>
+    <ButtonIcon onClick={() => deleteTodo(todo)}>
+      <AiOutlineDelete />
+    </ButtonIcon>
+  </TodoItem>
 );
 
 export default TodoListItem;
